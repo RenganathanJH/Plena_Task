21dd764539edd81dcc1803257a7347684e3cd17f
RenganathanTata <Renganathan.Hariharan@tataconsumer.com>
2024-01-23 23:32:59 +0530

    image added

---
+++ src/assets/images/svg/index.ts
@@ -0,0 +1,11 @@
+import FavouriteIcon from "./favorite.svg";
+import UnFavouriteIcon from "./unFavorite.svg";
+import PlusIcon from "./plus.svg";
+import EmptyImageIcon from "./emptyImage.svg";
+
+export const SVG = {
+    FavouriteIcon,
+    UnFavouriteIcon,
+    PlusIcon,
+    EmptyImageIcon
+}
